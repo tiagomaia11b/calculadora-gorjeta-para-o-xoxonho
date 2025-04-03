94302b9aeb5d94972cf7dded14b0ed76cb18acbb
tmaia5003 <tmaia5003@gmail.com>
2025-04-03 22:24:13 +0000

    nono commit do CSS e  segundo commit do js

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 let conta = 0
- let pessoas = 0
- lp
- const containput = document.querySelector("#conta")
- containput.addEventListener("input",receberValorConta )
+let pessoas = 0
+
+ const contaInput = document.querySelector("#conta")
+ contaInput.addEventListener("input", receberValorConta)
  
- function receberValorConta(evento){
-     conta = Number(evento.target.value)
-     
+ function receberValorConta(evento) {
+   conta = Number(evento.target.value)
  }
  
  const pessoasInput = document.querySelector("#pessoas")
  
  pessoasInput.addEventListener("input", receberQuantidadePessoa)
  
- function receberQuantidadePessoa (evento){
-     const paragrafoErro = document.querySelector(".pessoas #erro")
-     const divErro = document.querySelector(".pessoas .input-box")
- 
-     if(evento.target.value === "0"){
-         
-        console.log(paragrafoErro)
-        paragrafoErro.style.display = "block"
- 
-        divErro.setAttribute("id", "erro-div")
-     } else{
-         paragrafoErro.style.display = "none"
-         divErro.setAttribute("id", "")
-         pessoas = Number (evento.target.value)
-     }
- } 
+ function receberQuantidadePessoa(evento) {
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+  
+    if (evento.target.value === "0") {
+      paragrafoErro.style.display = "block"
+      divErro.setAttribute("id", "erro-div")
+    } else {
+      paragrafoErro.style.display = "none"
+      divErro.setAttribute("id", "")
+      pessoas = Number(evento.target.value)
+    }
+  }
+  
+  const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+  
+  botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", ReceberPorcentagemBotao)
+  })
+  
+  function ReceberPorcentagemBotao(evento) {
+  
+      botoesGorjeta.forEach(botao=> {
+          botao.classList.remove("botao-ativo")
+  
+          if(botao.value === evento.target.value)
+          botao.classList.add("botao-ativo")
+      })
+  
+    porcentagem = parseFloat(evento.target.value) / 100
+  }
+  
+  
+  Plugin
